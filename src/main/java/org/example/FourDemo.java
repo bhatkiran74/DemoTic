7007c11a754e178f559d8a83dcfefe752a82b51c
kiransing.bhat <92751608+bhatkiran1741026@users.noreply.github.com>
2022-11-07 23:29:31 +0530

    Update changes

--- src/main/java/org/example/FourDemo.java
+++ src/main/java/org/example/FourDemo.java
@@ -4,6 +4,5 @@ public class FourDemo {
     public static void main(String[] args) {
         System.out.println("Four demo");
          System.out.println("Four demo");
-         System.out.println("Four demo");
     }
 }
