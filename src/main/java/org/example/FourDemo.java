0d8b12dd6a3e22a83a014cb0cdcc08b6da47377c
kiransing.bhat <92751608+bhatkiran1741026@users.noreply.github.com>
2022-11-07 23:23:53 +0530

    Update four java class

--- src/main/java/org/example/FourDemo.java
+++ src/main/java/org/example/FourDemo.java
@@ -3,5 +3,7 @@ package org.example;
 public class FourDemo {
     public static void main(String[] args) {
         System.out.println("Four demo");
+         System.out.println("Four demo");
+         System.out.println("Four demo");
     }
 }
